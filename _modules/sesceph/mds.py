43cec3fc2e08459b1de34a852e902a707c2e4cf4
Owen Synge <osynge@suse.com>
2016-03-09 12:38:09 +0100

    sesceph.mds: now inherits from ctrl_rados_client
    Since much code is shared between mds and rgw
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- _modules/sesceph/mds.py
+++ _modules/sesceph/mds.py
@@ -1,13 +1,16 @@
+# Python imports
 import os
 import logging
 import json
 import shutil
 
+# Local imports
 import utils
 import constants
 import keyring
 import model
 import mdl_updater
+import rados_client
 
 
 log = logging.getLogger(__name__)
@@ -22,10 +25,10 @@ class Error(Exception):
         return ': '.join([doc] + [str(a) for a in self.args])
 
 
-class mds_ctrl(object):
+class mds_ctrl(rados_client.ctrl_rados_client):
     def __init__(self, **kwargs):
-        self.model = model.model(**kwargs)
-        self.model.init = "systemd"
+        super(mds_ctrl, self).__init__(**kwargs)
+        self.service_name = "ceph-mds"
         self.mds_name = kwargs.get("name")
         self.port = kwargs.get("port")
         self.addr = kwargs.get("addr")
