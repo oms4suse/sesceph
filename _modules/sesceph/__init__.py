3202d82387f53ac6578f41efb61659b059068cad
Owen Synge <osynge@suse.com>
2016-03-23 10:41:36 +0100

    _modules.sesceph.init:keyring_rgw_auth_add reuse keyring_auth_add
    Avoid code duplication and make things simpler.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- _modules/sesceph/__init__.py
+++ _modules/sesceph/__init__.py
@@ -875,9 +875,9 @@ def keyring_rgw_auth_add(**kwargs):
     cluster_name
         Set the cluster name. Defaults to "ceph".
     """
-    keyobj = keyring.keyring_facard()
-    keyobj.key_type = "rgw"
-    return keyobj.auth_add(**kwargs)
+    params = dict(kwargs)
+    params["keyring_type"] = "rgw"
+    return keyring_auth_add(**params)
 
 
 def keyring_rgw_auth_del(**kwargs):
