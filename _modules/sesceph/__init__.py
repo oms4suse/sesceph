2f9392f6d0af2c00c3c93155f06dd1d6eaf1cf6b
Owen Synge <osynge@suse.com>
2016-03-23 10:58:33 +0100

    _modules.sesceph.init:keyring_osd_auth_del reuse keyring_auth_del
    Avoid code duplication and make things simpler.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- _modules/sesceph/__init__.py
+++ _modules/sesceph/__init__.py
@@ -658,9 +658,9 @@ def keyring_osd_auth_del(**kwargs):
     cluster_name
         Set the cluster name. Defaults to "ceph".
     """
-    keyobj = keyring.keyring_facard()
-    keyobj.key_type = "osd"
-    return keyobj.auth_del(**kwargs)
+    params = dict(kwargs)
+    params["keyring_type"] = "osd"
+    return keyring_auth_del(**params)
 
 
 def keyring_osd_purge(**kwargs):
