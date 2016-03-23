65b5c95d46ff2b2541369238dd1c58cfe135653e
Owen Synge <osynge@suse.com>
2016-03-23 10:38:05 +0100

    _modules.sesceph.init:keyring_osd_auth_add reuse keyring_auth_add
    Avoid code duplication and make things simpler.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- _modules/sesceph/__init__.py
+++ _modules/sesceph/__init__.py
@@ -636,9 +636,9 @@ def keyring_osd_auth_add(**kwargs):
     cluster_name
         Set the cluster name. Defaults to "ceph".
     """
-    keyobj = keyring.keyring_facard()
-    keyobj.key_type = "osd"
-    return keyobj.auth_add(**kwargs)
+    params = dict(kwargs)
+    params["keyring_type"] = "osd"
+    return keyring_auth_add(**params)
 
 
 def keyring_osd_auth_del(**kwargs):
